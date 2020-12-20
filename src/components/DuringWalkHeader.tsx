a7a2bd7e14b225f28ecdbf340c7b212b3e2819aa
Victoria ❤ <vickyakeke@gmail.com>
2020-12-21 00:16:19 +0300

    Adding modal pop up to add bee cast data when on walk

--- src/components/DuringWalkHeader.tsx
+++ src/components/DuringWalkHeader.tsx
@@ -4,6 +4,8 @@ import {IonButton, IonCard, IonCardContent, IonCol, IonGrid, IonRow, IonRouterLi
 /*import './LoginPage.css';*/
 import "../pages/Default.css";
 import '../theme/variables.css';
+import BeeSpecies from './BeeSpecies';
+import ReactDOM from 'react-dom';
 /* DuringWalkHeader - Is the template for the header 
     of the DuringWalk page that collects data during the walk
 */
@@ -12,10 +14,36 @@ interface ContainerProps {
 
 const DuringWalkHeader: React.FC<ContainerProps> = () => {
   /*<input type="submit" value="Log In" id="submit"></input>*/
+  const RepeatSpecies: React.FC<ContainerProps> = () => {
+    return(
+      <><BeeSpecies content="Bee Species Name"/>
+        <BeeSpecies content="Bee Species Name" />
+        <BeeSpecies content="Bee Species Name"/>
+        <BeeSpecies content="Bee Species Name"/></> 
+    )
+  };
+
+  const RepeatRecords: React.FC<ContainerProps> = () => {
+    return(
+      <><BeeSpecies content="Bee Records"/>
+        <BeeSpecies content="Bee Records" />
+        <BeeSpecies content="Bee Records"/>
+        <BeeSpecies content="Bee Records"/></> 
+    )
+  };
+  const showRecords=(truth: boolean)=>{
+      var body = document.getElementById("main")
+      if (truth){
+        ReactDOM.render(
+          <RepeatSpecies/> , body)
+      }else{
+        ReactDOM.render(
+          <RepeatRecords/> , body)
+      }     
+  }
   return (   
-    <>
-       
-        <IonButton className="whitebackground">
+    <>       
+        <IonButton color="light" href="/start/duringwalk/manual">
           Add Manually
         </IonButton>
 
@@ -23,10 +51,11 @@ const DuringWalkHeader: React.FC<ContainerProps> = () => {
               Submit
           </IonButton>
 
-          <IonToolbar className="toolbar">
+          <IonToolbar className="toolbar" >
                 <IonSegment value="all">
-                    <IonSegmentButton value="all">Enter Records</IonSegmentButton>
-                    <IonSegmentButton value="favorites">Check Records</IonSegmentButton>
+                    <IonSegmentButton value="all" onClick={()=>showRecords(true)}>Enter Records</IonSegmentButton>
+                    
+                    <IonSegmentButton value="favorites" onClick={()=>showRecords(false)}>Check Records</IonSegmentButton>
                 </IonSegment>
           </IonToolbar> 
     </>  
