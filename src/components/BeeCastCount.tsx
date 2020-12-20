6194b24a986c23822fa0032be8a6775ccf54ef7d
Victoria ❤ <vickyakeke@gmail.com>
2020-12-21 00:13:14 +0300

    Counting individual bee casts on a different component

---
+++ src/components/BeeCastCount.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { IonCol,  IonRow, IonFab, IonFabButton, IonIcon} from '@ionic/react';
+
+import "../pages/Default.css";
+import { add, removeOutline } from 'ionicons/icons';
+
+/* BeeCastCount - Template for the body of the DuringWalk page 
+that collects data during the walk 
+              - Contains the counter of each bee cast 
+*/
+interface ContainerProps {  
+    cast : string 
+}
+
+const BeeCastCount: React.FC<ContainerProps> = (props) => {
+  return ( 
+      <>  
+                <IonRow>
+                    <IonCol size="6">
+                        <h1>{props.cast}</h1>
+                    </IonCol>
+
+                    <IonCol size="3">
+                        <IonFab vertical="top" horizontal="end" >
+                            <IonFabButton>
+                                <IonIcon icon={add} />
+                            </IonFabButton>                           
+                        </IonFab>
+                        
+                        
+                    </IonCol>
+
+                    <IonCol size="3">
+                        <IonFab vertical="top" horizontal="end" >
+
+                            <IonFabButton>                                
+                                <IonIcon icon={removeOutline} />
+                            </IonFabButton>                            
+                        </IonFab>                        
+                        
+                    </IonCol>
+                    
+                </IonRow>
+                
+            </>
+            );
+    
+        };
+        
+export default BeeCastCount;
