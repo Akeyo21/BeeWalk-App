6663411c5ee92853114a098bc508f7c435b159f3
Victoria ❤ <vickyakeke@gmail.com>
2020-12-21 00:15:09 +0300

    Add a property to set whether it is for the species or records entered page

--- src/components/BeeSpecies.tsx
+++ src/components/BeeSpecies.tsx
@@ -1,36 +1,71 @@
-import React from 'react';
-import {IonButton, IonCard, IonCardContent, IonCol, IonGrid, IonRow, IonRouterLink} from '@ionic/react';
+import React, { useState } from 'react';
+import {IonButton, IonCard, IonCardContent, IonCol, IonGrid, IonRow, IonRouterLink, IonModal, IonPicker, IonFab, IonFabButton, IonIcon, IonBackdrop, IonItem, IonItemOption, IonItemOptions, IonItemSliding, IonLabel} from '@ionic/react';
 
 import "../pages/Default.css";
+import BeeCastCount from "./BeeCastCount"
+import { add, removeOutline } from 'ionicons/icons';
 
 /* BeeSpecies - Template for the body of the DuringWalk page t
 hat collects data during the walk 
               - Contains the specific bee species info 
 */
-interface ContainerProps {   
+interface ContainerProps {  
+    content: string
+    
 }
 
-const BeeSpecies: React.FC<ContainerProps> = () => {
+const BeeSpecies: React.FC<ContainerProps> = (props) => {
+    const [showModal, setShowModal] = useState(false);
   /*<input type="submit" value="Log In" id="submit"></input>*/
+    /*console.log(props.records)
+    if(props.records){
+        return(
+            console.log("Here")
+        );
+    }else{*/
   return (   
+      
     <>
-        <IonCard className="card ">
-            <IonCardContent className="nopadding">
-                <IonGrid className="nopadding">
-                    <IonRow>
-                        <IonCol size="8" className="nopadding">
-                            <img src="assets/images/bee.jpg"></img>
-                        </IonCol>
+        <IonModal isOpen={showModal} cssClass='modal' showBackdrop>
+            
+            <h1>Bee Species Name</h1>
+            
+            <IonGrid className="grid">
+                <BeeCastCount cast="Queen"/>
+                <BeeCastCount cast="Worker"/>
+                <BeeCastCount cast="Unknown"/>
+            </IonGrid>
+            <IonButton onClick={() => setShowModal(false)}>Close Modal</IonButton>
+            
+        </IonModal>
+        <IonItemSliding>
+            <IonItem className="nopadding nomargin">
+                <IonCard className="card " onClick={() => setShowModal(true)}>
+                    <IonCardContent className="nopadding">
+                        <IonGrid className="nopadding">
+                            <IonRow>
+                                <IonCol size="8" className="nopadding">
+                                    <img src="assets/images/bee.jpg"></img>
+                                </IonCol>
 
-                        <IonCol size="4">
-                            <h1 className="ion-padding-start ion-padding-top">Bee Species name</h1>
-                        </IonCol>
-                    </IonRow>
-                </IonGrid>
-            </IonCardContent>
-        </IonCard>      
+                                <IonCol size="4">
+                                    <h1 className="ion-padding-start ion-padding-top">{props.content}</h1>
+                                </IonCol>
+                            </IonRow>
+                        </IonGrid>
+                    </IonCardContent>
+                </IonCard>      
+            </IonItem>
+
+            <IonItemOptions side="end">
+            <IonItemOption color="warning" expandable>
+                    <h3>Bee Cast Info</h3>
+                </IonItemOption>
+            </IonItemOptions>
+    </IonItemSliding>
     </>  
   );
+    
 };
 
 export default BeeSpecies;
