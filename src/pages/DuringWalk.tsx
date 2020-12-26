e8b0736b34e7fa072e8a76adfe9f57993c141c54
Victoria ❤ <vickyakeke@gmail.com>
2020-12-26 20:17:40 +0300

    Changes to allow for header to be visible on phone

--- src/pages/DuringWalk.tsx
+++ src/pages/DuringWalk.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 /*import '../components/ExploreContainer.css';*/
-import { IonButton,  IonCard,  IonCardContent,  IonCol,  IonContent, IonFooter, IonGrid, IonHeader, IonInput, IonPage, IonRouterLink, IonRouterOutlet, IonRow, IonSegment, IonSegmentButton, IonText, IonToolbar } from '@ionic/react';
+import { IonAlert, IonButton,  IonCard,  IonCardContent,  IonCol,  IonContent, IonFooter, IonGrid, IonHeader, IonInput, IonPage, IonRouterLink, IonRouterOutlet, IonRow, IonSegment, IonSegmentButton, IonText, IonToolbar } from '@ionic/react';
 /*import '../components/LoginPage.css';
 import './PreWalk.css';*/
 import './Default.css';
 import DuringWalkHeader from '../components/DuringWalkHeader'
 
+import '../theme/variables.css';
 import ManualData from "./ManualData";
-import {Route} from 'react-router-dom';
+import {Redirect, Route} from 'react-router-dom';
 import BeeSpecies from '../components/BeeSpecies'
+import ReactDOM from 'react-dom';
+import Postlogin from './Postlogin';
 
 /*
     DuringWalk - page that collects data during walk
@@ -17,30 +20,98 @@ interface ContainerProps {
 }
 
 const DuringWalk: React.FC<ContainerProps> = () => {
-  return (
+  const RepeatSpecies: React.FC<ContainerProps> = () => {
+    return(
+      <><BeeSpecies content="Bee Species Name"/>
+        <BeeSpecies content="Bee Species Name" />
+        <BeeSpecies content="Bee Species Name"/>
+        <BeeSpecies content="Bee Species Name"/></> 
+    )
+  };
+
+  const RepeatRecords: React.FC<ContainerProps> = () => {
+    return(
+      <><BeeSpecies content="Bee Records"/>
+        <BeeSpecies content="Bee Records" />
+        <BeeSpecies content="Bee Records"/>
+        <BeeSpecies content="Bee Records"/></> 
+    )
+  };
+  const showRecords=(truth: boolean)=>{
+      var body = document.getElementById("main")
+      if (truth){
+        ReactDOM.render(
+          <RepeatSpecies/> , body)
+      }else{
+        ReactDOM.render(
+          <RepeatRecords/> , body)
+      }     
+  }
+  const [showAlert1, setShowAlert1] = useState(false);
+
+  const done= () =>{
+    return(
     <><><IonRouterOutlet>
+        <Route path="/frontpage" component={Postlogin} />
+
+      </IonRouterOutlet></>
+        <Redirect to="/frontpage"></Redirect></>
+    )
+  }
+  return (
+    <><></><><IonRouterOutlet>
       <Route path="/start/duringwalk/manual" component={ManualData} />
     </IonRouterOutlet></>
       <IonPage>
-        <IonHeader>
-        </IonHeader>
-        <IonContent fullscreen className="whitebackground ">
-            <IonHeader collapse="condense" className= "switch">
-               <DuringWalkHeader/>                               
+          <IonContent fullscreen className="whitebackground ">
+            <IonHeader className="switch">
+              <IonButton color="light" href="/start/duringwalk/manual">
+                Add Manually
+              </IonButton>
+
+              <IonButton onClick={() => setShowAlert1(true)} className="whitebackground ion-float-right">
+                Submit
+              </IonButton>
+              <IonAlert
+                isOpen={showAlert1}
+                onDidDismiss={() => setShowAlert1(false)}
+                cssClass='submitalert'
+                header={'Submission'}
+                message={'Do you wish to submit your data?'}
+                buttons={[
+                  {
+                    text: 'OK',
+                    handler: () => {
+                      done();
+                    }
+                  },
+
+                  {
+                    text: 'Cancel',
+                    role: 'cancel'
+                  }
+                ]} />
+
+              <IonToolbar className="toolbar">
+                <IonSegment value="all">
+                  <IonSegmentButton value="all" onClick={() => showRecords(true)}>Enter Records</IonSegmentButton>
+                  <IonSegmentButton value="favorites" onClick={() => showRecords(false)}>Check Records</IonSegmentButton>
+                </IonSegment>
+              </IonToolbar>
+
             </IonHeader>
-            <div className="datacontent" id="main">              
-              <BeeSpecies content="Bee Species Name" /> 
-              <BeeSpecies content="Bee Species Name"/> 
-              <BeeSpecies content="Bee Species Name"/> 
-              <BeeSpecies content="Bee Species Name"/> 
-              <BeeSpecies content="Bee Species Name"/> 
-              <BeeSpecies  content="Bee Species Name"/> 
+            <div className="datacontent" id="main">
+              <BeeSpecies content="Bee Species Name" />
+              <BeeSpecies content="Bee Species Name" />
+              <BeeSpecies content="Bee Species Name" />
+              <BeeSpecies content="Bee Species Name" />
+              <BeeSpecies content="Bee Species Name" />
+              <BeeSpecies content="Bee Species Name" />
 
             </div>
-            
 
-        </IonContent>
-      </IonPage></>
+
+          </IonContent> </IonPage></>
     
     
   );
