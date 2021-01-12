781e08ad4d9ce5dc170c0046da84bd9a78619849
Victoria ❤ <vickyakeke@gmail.com>
2021-01-13 00:08:09 +0300

    sending data collected in bee species page to the bee records page

--- src/components/BeeSpecies.tsx
+++ src/components/BeeSpecies.tsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import {IonButton, IonCard, IonCardContent, IonCol, IonGrid, IonRow, IonRouterLink, IonModal, IonPicker, IonFab, IonFabButton, IonIcon, IonBackdrop, IonItem, IonItemOption, IonItemOptions, IonItemSliding, IonLabel} from '@ionic/react';
 
 import "../pages/Default.css";
 import BeeCastCount from "./BeeCastCount"
-import { add, removeOutline } from 'ionicons/icons';
+import { add, addCircle, removeCircle, removeOutline } from 'ionicons/icons';
+import { Context } from '../pages/DuringWalk';
+import { State } from 'ionicons/dist/types/stencil-public-runtime';
 
 /* BeeSpecies - Template for the body of the DuringWalk page t
 hat collects data during the walk 
@@ -14,16 +16,45 @@ interface ContainerProps {
     
 }
 
-const BeeSpecies: React.FC<ContainerProps> = (props) => {
+const BeeSpeciesFile: React.FC<ContainerProps> = (props) => {
     const [showModal, setShowModal] = useState(false);
-    
-    
+    const {beedata, addBeeData} = useContext(Context);
+    class BeeSpecies{
+        name: String;
+        queen: number;
+        worker: number;
+        unknown: number;
+        constructor(name: String, queen: number, worker: number, unknown:number){
+            this.name = name;
+            this.queen = queen;
+            this.worker = worker;
+            this.unknown = unknown;
+        }
+    }
+    function addBeeDataToList(){
+       
+        setShowModal(false)
+        var queenvalue = document.getElementsByClassName("number")[0].textContent
+        var workervalue = document.getElementsByClassName("number")[1].textContent
+        var unknownvalue = document.getElementsByClassName("number")[2].textContent
+
+        if (queenvalue && workervalue && unknownvalue ){
+            let beeEntered = new BeeSpecies(props.content, parseInt(queenvalue),
+            parseInt(workervalue), parseInt(unknownvalue))
+            /*beedatalist.push(beeEntered)*/
+            var beedatalist: BeeSpecies[] = beedata.push(beeEntered)
+            /*console.log(beedatalist)*/
+            addBeeData(beedatalist)
+            
+        }
+    }
+    console.log(beedata.length)
   return (   
       
     <>
         <IonModal isOpen={showModal} cssClass='modal' showBackdrop backdrop-dismiss={false}>
             
-            <h1>Bee Species Name</h1>
+            <h1>{props.content}</h1>
             
             <IonGrid className="grid">
                 <BeeCastCount cast="Queen"/>
@@ -34,7 +65,7 @@ const BeeSpecies: React.FC<ContainerProps> = (props) => {
             <IonGrid>
                 <IonRow>
                     <IonCol size="6">
-                        <IonButton onClick={() => setShowModal(false)} color="dark">Add Record</IonButton>
+                        <IonButton onClick={() => addBeeDataToList()} color="dark">Add Record</IonButton>
                     </IonCol>
 
                     <IonCol size="4">
@@ -74,5 +105,4 @@ const BeeSpecies: React.FC<ContainerProps> = (props) => {
   );
     
 };
-
-export default BeeSpecies;
+export default BeeSpeciesFile;
