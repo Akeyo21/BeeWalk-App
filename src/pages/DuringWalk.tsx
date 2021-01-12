6370ff492787156c50d04d2a581248e52d56088a
Victoria ❤ <vickyakeke@gmail.com>
2021-01-13 00:08:43 +0300

    Allow for sending in data between the child components

--- src/pages/DuringWalk.tsx
+++ src/pages/DuringWalk.tsx
@@ -9,9 +9,8 @@ import DuringWalkHeader from '../components/DuringWalkHeader'
 import '../theme/variables.css';
 import ManualData from "./ManualData";
 import {Redirect, Route} from 'react-router-dom';
-import BeeSpecies from '../components/BeeSpecies'
+import BeeSpeciesFile from '../components/BeeSpecies'
 import BeeRecords from '../components/BeeRecords'
-
 import ReactDOM from 'react-dom';
 import Postlogin from './Postlogin';
 import { ReactComponent } from '*.svg';
@@ -29,25 +28,66 @@ interface ContainerProps {
       count: 0
     };
   }*/
-//const DuringWalk: React.FC<ContainerProps> = () => {
-export default class DuringWalk extends React.Component{
-  render(){
+const DuringWalk: React.FC<ContainerProps> = () => {
+//export default class DuringWalk extends React.Component{
+  
+  const [beedata, addBeeData] = useState([]as any)
   const RepeatSpecies: React.FC<ContainerProps> = () => {
     return(
-      <><BeeSpecies content="Bee Species Name 1"/>
-        <BeeSpecies content="Bee Species Name 2" />
-        <BeeSpecies content="Bee Species Name 3"/>
-        <BeeSpecies content="Bee Species Name 4"/></> 
+      <>
+      <Context.Provider value={{beedata:beedata as any , addBeeData: (list) => {return list} }}>
+      <BeeSpeciesFile content="Bee Species Name 1" />
+      <BeeSpeciesFile content="Bee Species Name 2" />
+      <BeeSpeciesFile content="Bee Species Name 3" />
+      <BeeSpeciesFile content="Bee Species Name 4" />
+      <BeeSpeciesFile content="Bee Species Name 5" />
+      <BeeSpeciesFile content="Bee Species Name 6" />
+        </Context.Provider></>
     )
   };
-
+  class BeeSpecies{
+    name: String;
+    queen: number;
+    worker: number;
+    unknown: number;
+    constructor(name: String, queen: number, worker: number, unknown:number){
+        this.name = name;
+        this.queen = queen;
+        this.worker = worker;
+        this.unknown = unknown;
+    }
+}
+  const removeDuplicates=(list: BeeSpecies[])=>{
+    var arrayreturned =[...list]
+    for(var index=0;index<list.length;index++){
+      for (var second=index+1;second<list.length;second++){
+        if (list[index].name == list[second].name){
+          arrayreturned.splice(index)
+          arrayreturned.splice(second)
+          var queen = list[index].queen + list[second].queen
+          var worker = list[index].worker+ list[second].worker
+          var unknown = list[index].unknown +list[second].unknown
+          var beecombined = new BeeSpecies(list[index].name, queen, worker, unknown )
+          arrayreturned.push(beecombined)
+        } 
+      }
+    }
+    return arrayreturned
+  };
   const RepeatRecords: React.FC<ContainerProps> = () => {
     return(
-      <><BeeRecords content="Bee Records"/>
-        <BeeRecords content="Bee Records"/>
-        <BeeRecords content="Bee Records"/>
-        <BeeRecords content="Bee Records"/>
-        <BeeRecords content="Bee Records"/></>
+      
+      <><Context.Provider value={{ beedata: beedata, addBeeData: (list) => {return list } }}>
+        
+        
+      {console.log(beedata)}
+      {console.log(removeDuplicates(beedata))}
+        {removeDuplicates(beedata).map((item: any) => (
+      
+      <BeeRecords name={item.name} queen={item.queen} worker={item.worker} unknown={item.unknown}/>
+        ))}
+        {console.log(beedata.length)}
+        </Context.Provider></>
     )
   };
   const showRecords=(truth: boolean)=>{
@@ -59,16 +99,22 @@ export default class DuringWalk extends React.Component{
           <RepeatSpecies/> , body)
       }else{
         ReactDOM.render(
-          <RepeatRecords/> , body)
+          <><RepeatRecords />            
+          
+            </> , body)
       }     
   }
+  
   const [showAlert1, setShowAlert1] = useState(false);
-  const [redirectHome, setRedirectHome] = useState(false)
+  const [redirectHome, setRedirectHome] = useState(false);
   /*Change between pages when ok is clicked in alert*/
   if (redirectHome==true){
     return <Redirect to='/frontpage' />
   }
+  console.log(beedata.length)
+  console.log("Hello")
   return (
+    
     <><></><><IonRouterOutlet>
       <Route path="/start/duringwalk/manual" component={ManualData} />
     </IonRouterOutlet></>
@@ -111,14 +157,8 @@ export default class DuringWalk extends React.Component{
               </IonToolbar>
 
             </IonHeader>
-            <div className="datacontent" id="main">
-              <BeeSpecies content="Bee Species Name 1" />
-              <BeeSpecies content="Bee Species Name 2" />
-              <BeeSpecies content="Bee Species Name 3" />
-              <BeeSpecies content="Bee Species Name 4" />
-              <BeeSpecies content="Bee Species Name 5" />
-              <BeeSpecies content="Bee Species Name 6" />
-
+            <div className="datacontent" id="main">              
+                <RepeatSpecies/>
             </div>
 
 
@@ -127,6 +167,5 @@ export default class DuringWalk extends React.Component{
     
   );
 };
-}
-export const MContext = React.createContext("");  
-//export default DuringWalk;
+export const Context = React.createContext({ beedata:[] as any, addBeeData: (list: any) => {return list} });
+export default DuringWalk;
