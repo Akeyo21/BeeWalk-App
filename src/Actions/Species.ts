fa08cb14af00764abadf36bad58b959e13698ba8
Victoria ❤ <vickyakeke@gmail.com>
2021-01-29 23:00:13 +0300

    Adding in actions for the redux

---
+++ src/Actions/Species.ts
@@ -0,0 +1,19 @@
+import { BeeSpecies } from "../Reducers/SpeciesReducer"
+
+// Create Redux action types
+export const SELECT_BEE_SPECIES = 'SELECT_BEE_SPECIES'
+export const SEND_BEE_SPECIES = 'SEND_BEE_SPECIES'
+
+// Create Redux action creators that return an action
+export const selectBeeSpecies = (speciesname: BeeSpecies) => ({
+    type: SELECT_BEE_SPECIES,
+    payload: speciesname,
+  })
+
+export const sendBeeSpecies = ()=>({
+    type: SEND_BEE_SPECIES,
+})
+/*
+  export function selectedSpecies(species){
+      dispatch(selectBeeSpecies(species))
+  }*/
