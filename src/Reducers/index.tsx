0d478e8de3aa9a5dd1946500bad7a2bc264ad6ce
Victoria ❤ <vickyakeke@gmail.com>
2021-01-31 00:52:59 +0300

    Adding in the records reducers

--- src/Reducers/index.tsx
+++ src/Reducers/index.tsx
@@ -2,9 +2,11 @@ import {combineReducers} from 'redux'
 
 //import postsReducer from './postsReducer'
 import speciesReducer from './SpeciesReducer'
+import recordsReducer from './RecordsReducer'
 const rootReducer = combineReducers({
   //posts: postsReducer,
   species: speciesReducer,
+  records: recordsReducer,
 })
 
 export default rootReducer
