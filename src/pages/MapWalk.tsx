c033428090e756d3568162a1b97ff6b9aad05bbd
Victoria ❤ <vickyakeke@gmail.com>
2021-02-04 23:43:51 +0300

    Dispatch the number of photos in the store already

--- src/pages/MapWalk.tsx
+++ src/pages/MapWalk.tsx
@@ -1,4 +1,4 @@
-import React, { constructor, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../components/ExploreContainer.css';
 import { IonAlert, IonBackButton, IonButton,  IonButtons,  IonCol,  IonContent, IonDatetime, IonGrid, IonHeader, IonInput, IonItem, IonLabel, IonList, IonLoading, IonPage, IonRouterOutlet, IonRow, IonSelect, IonSelectOption, IonText, IonToolbar } from '@ionic/react';
 /*import '../components/LoginPage.css';*/
@@ -23,6 +23,11 @@ import {
     Environment
   } from '@ionic-native/google-maps';
 import L from 'leaflet';
+import { useDispatch } from 'react-redux';
+import { addRecord, resetRecords } from '../Actions/Records';
+import { photosPresent } from '../Actions/Photos';
+import { usePhotoGallery } from './Camera';
+
 
     /*PreWalk - opens the first page containing details 
     required prior to starting the beewalk
@@ -36,8 +41,13 @@ const MapWalk: React.FC<ContainerProps> = (props) => {
    const [map,setMap] = useState<typeof MapContainer>()
    
    
+ const dispatch = useDispatch()
   const [redirectRecords, setRedirectRecords] = useState(false)
-
+  let {  photos,clearPhotos} = usePhotoGallery();
+  useEffect(()=>{
+    dispatch(photosPresent(photos.length))
+    //document.title = `You clicked ${count} times`;
+  },[photos.length]);
       const [showAlert1, setShowAlert1] = useState(false);
       const [redirectHome, setRedirectHome] = useState(false);
       /*Change between pages when ok is clicked in alert
@@ -57,12 +67,19 @@ const MapWalk: React.FC<ContainerProps> = (props) => {
      
   AIzaSyAmfNAhG-WbTTCN-7JmHApcvr9e1tYirGw - API key
   */
+ const clearRecords=()=>{
+  dispatch(resetRecords())
+  //clearPhotos()
+  setShowAlert1(true)
+ }
   if (redirectHome==true){
     return <Redirect to='/frontpage' />
   } 
   if (redirectRecords ==true){
     return <Redirect to='/start/records'/>
   }
+  
+  
   console.log("In the map page")
   return (
     
@@ -70,9 +87,9 @@ const MapWalk: React.FC<ContainerProps> = (props) => {
     <IonRouterOutlet>
           <Route exact path='/start/records' component={Records} />
   </IonRouterOutlet>
-     <IonButton color="warning" size="large" className="buttons" shape="round" expand="block" routerLink="/start/recordform">Record without photo/video</IonButton>
-    <IonButton color="warning" size="large" className="buttons" shape="round" expand="block" onClick={() => setRedirectRecords(true)}>Records entered</IonButton>
-    <IonButton color="warning" size="large" className="buttons" shape="round" expand="block" onClick={() => setShowAlert1(true)}>
+     <IonButton color="warning" size="large" className="buttons" shape="round" expand="block" href="/start/recordform" >Record without photo/video</IonButton>
+    <IonButton color="warning" size="large" className="buttons" shape="round" expand="block" href="/start/records" >Records entered</IonButton>
+    <IonButton color="warning" size="large" className="buttons" shape="round" expand="block" onClick={() => clearRecords()}>
       Save Records
       </IonButton>
       <IonAlert
