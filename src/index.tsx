7130b998899087435bf59436fa114a52debd7f69
Victoria ❤ <vickyakeke@gmail.com>
2021-02-04 23:42:40 +0300

    Add in persistent store

--- src/index.tsx
+++ src/index.tsx
@@ -9,11 +9,34 @@ import thunk from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import rootReducer from './Reducers/index'
 // Call the element loader after the platform has been bootstrapped
+//import { createStore } from 'redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+
+//import rootReducer from './reducers'
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+/*export default () => {
+  let store = createStore(persistedReducer)
+  let persistor = persistStore(store)
+  return { store, persistor }
+}*/
 defineCustomElements(window);
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+//const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+const store = createStore(persistedReducer, composeWithDevTools(applyMiddleware(thunk)))
+const persistor = persistStore(store)
 ReactDOM.render(
 <Provider store={store}>
+<PersistGate loading={null} persistor={persistor}>
     <App/>
+    </PersistGate>
 </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
