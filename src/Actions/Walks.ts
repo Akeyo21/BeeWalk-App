179c4ffa85131dd853c4c2b5ed0867b57925688b
Victoria ❤ <vickyakeke@gmail.com>
2021-02-06 01:36:40 +0300

    Creating actions for both walk situations-before data collection and after

---
+++ src/Actions/Walks.ts
@@ -0,0 +1,29 @@
+import { Walk } from "../Reducers/WalksBeforeReducer"
+import {UpdatedWalk} from '../Reducers/WalksReducer'
+// Create Redux action types
+export const SET_WALK = 'SET_WALK'
+export const RESET_WALK = 'RESET_WALK'
+export const ADD_WALK = 'ADD_WALK'
+export const RESET_WALKS = 'RESET_WALKS'
+
+//export const RESET_RECORDS = 'RESET_RECORDS'
+// Create Redux action creators that return an action
+export const setWalk = (walk: Walk) => ({
+    type: SET_WALK,
+    payload: walk,
+  })
+
+  export const resetWalk = () => ({
+    type: RESET_WALK,
+  })
+
+export const addWalk=(walk:UpdatedWalk)=>({
+    type:ADD_WALK,
+    payload:walk,
+})
+
+export const resetWalks = () => ({
+    type: RESET_WALKS,
+  })
+
+
