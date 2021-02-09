094f655d4054e00ac17403c5975cbd1810d8dc32
Victoria ❤ <vickyakeke@gmail.com>
2021-02-09 22:18:28 +0300

    Check if memoryfull is true

--- src/pages/MapWalk.tsx
+++ src/pages/MapWalk.tsx
@@ -26,10 +26,11 @@ import L from 'leaflet';
 import { connect, useDispatch } from 'react-redux';
 import {  resetRecords } from '../Actions/Records';
 import { photosPresent } from '../Actions/Photos';
-import { usePhotoGallery } from './Camera';
+import { usePhotoGallery, promiseState } from './Camera';
 import { Walk } from '../Reducers/WalksBeforeReducer';
 import { addWalk, resetWalk } from '../Actions/Walks';
 import { UpdatedWalk } from '../Reducers/WalksReducer';
+import { setFalse } from '../Actions/MemoryFull';
 
     /*PreWalk - opens the first page containing details 
     required prior to starting the beewalk
@@ -38,23 +39,26 @@ interface ContainerProps {
   google: any
   records:[]|any
   walk:any
+  memoryFull:boolean
 }
 
 const MapWalk: React.FC<ContainerProps> = (props) => {
   
    const [map,setMap] = useState<typeof MapContainer>()
    
+  let { photos, takePhoto } = usePhotoGallery();
    console.log(props.walk)
    
  const dispatch = useDispatch()
   const [redirectRecords, setRedirectRecords] = useState(false)
-  let {  photos,clearPhotos} = usePhotoGallery();
   /*useEffect(()=>{
     dispatch(photosPresent(photos.length))
     //document.title = `You clicked ${count} times`;
   },[photos.length]);*/
       const [showAlert1, setShowAlert1] = useState(false);
       const [redirectHome, setRedirectHome] = useState(false);
+      
+ const [memoryAlert, setMemoryAlert] = useState(false)
       /*Change between pages when ok is clicked in alert
       
   AIzaSyAmfNAhG-WbTTCN-7JmHApcvr9e1tYirGw - API key
@@ -89,14 +93,51 @@ console.log(recordsEntered)
   setShowAlert1(true)
   setRedirectHome(true)
  }
+ const [enterRecord, setEnterRecord] = useState(false);
+
   if (redirectHome==true){
     return <Redirect to='/frontpage' />
   } 
   if (redirectRecords ==true){
     return <Redirect to='/start/records'/>
   }
+  if(enterRecord){
+    return <Redirect to='/start/recordform'/>
+  }
   
-  
+  const record=()=>{
+    dispatch(photosPresent(photos.length))
+    setRedirectRecords(true)
+    
+  }
+ const picha = ()=>{
+   
+     takePhoto()
+    /*console.log(s)
+    console.log()
+    promiseState(s, function(state) {
+          
+      if(state=='rejected'){
+        console.log("reject")
+        setShowAlert1(true)
+      }
+      })*/
+ }
+  const take=async ()=>{
+    dispatch(photosPresent(photos.length))
+    let s = await picha()
+    if(photos.length==0){
+      dispatch(setFalse())
+    } 
+    console.log(props.memoryFull)
+    if(props.memoryFull==true){
+      setMemoryAlert(true)
+    }else{
+    setEnterRecord(true)
+
+    }
+    
+  }
   console.log("In the map page")
   return (
     
@@ -104,8 +145,10 @@ console.log(recordsEntered)
     <IonRouterOutlet>
           <Route exact path='/start/records' component={Records} />
   </IonRouterOutlet>
+  <IonButton color="warning" size="large" className="buttons" shape="round" expand="block" onClick={()=>(take())}>Record with photo/video</IonButton>
+    
      <IonButton color="warning" size="large" className="buttons" shape="round" expand="block" href="/start/recordform" >Record without photo/video</IonButton>
-    <IonButton color="warning" size="large" className="buttons" shape="round" expand="block" href="/start/records" >Records entered</IonButton>
+    <IonButton color="warning" size="large" className="buttons" shape="round" expand="block" onClick={()=>(record())}>Records entered</IonButton>
     <IonButton color="warning" size="large" className="buttons" shape="round" expand="block" onClick={() => setShowAlert1(true)}>
       Save Records
       </IonButton>
@@ -128,6 +171,23 @@ console.log(recordsEntered)
                     role: 'cancel'
                   }
                 ]} />
+
+<IonAlert
+                isOpen={memoryAlert}
+                onDidDismiss={() => setMemoryAlert(false)}
+                cssClass='submitalert'
+                header={'Memory Full'}
+                message={'Photos cannot be taken with full memory'}
+                buttons={[
+                  {
+                    text: 'OK'
+                  },
+
+                  {
+                    text: 'Cancel',
+                    role: 'cancel'
+                  }
+                ]} />
   
       </>
   
@@ -141,7 +201,8 @@ console.log(recordsEntered)
 const mapStateToProps = function(state: any) {
   return {
     records:state.records,
-    walk:state.walk
+    walk:state.walk,
+    memoryFull: state.memoryFull
   }
 }
 
