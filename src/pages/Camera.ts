e974dcba72d419a89d71f1721f28f4ddd62770b2
Victoria ❤ <vickyakeke@gmail.com>
2021-02-09 22:16:23 +0300

    Check if there is localstorage error and make memoryfull true

--- src/pages/Camera.ts
+++ src/pages/Camera.ts
@@ -3,7 +3,10 @@ import { useCamera } from '@ionic/react-hooks/camera';
 import { useFilesystem, base64FromPath } from '@ionic/react-hooks/filesystem';
 import { useStorage } from '@ionic/react-hooks/storage';
 import { isPlatform } from '@ionic/react';
-import { CameraResultType, CameraSource, CameraPhoto, Capacitor, FilesystemDirectory } from "@capacitor/core";
+import { CameraResultType, CameraSource, CameraPhoto, Capacitor, FilesystemDirectory, Camera } from "@capacitor/core";
+import { useDispatch, useSelector } from "react-redux";
+import { setTrue } from "../Actions/MemoryFull";
+import { RootState } from "../Reducers";
 export interface Photo {
   filepath: string;
   webviewPath?: string;
@@ -15,10 +18,33 @@ const clear=async ()=>{
   const photosString = await set('photos', clear());
 }
 }*/
+export function promiseState(promise: any, callback: (arg0: string) => any) {
+  // Symbols and RegExps are never content-equal
+  var uniqueValue = window['Symbol'] ? Symbol('unique') : /unique/
+
+  function notifyPendingOrResolved(value: symbol | RegExp) {
+    if (value === uniqueValue) {
+      return callback('pending')
+    } else {
+      return callback('fulfilled')
+    }
+  }
+
+  function notifyRejected(reason: any) {
+    return callback('rejected')
+  }
+  
+  var race = [promise, Promise.resolve(uniqueValue)]
+  Promise.race(race).then(notifyPendingOrResolved, notifyRejected)
+}
+
 export function usePhotoGallery() {
   const { deleteFile, getUri, readFile, writeFile } = useFilesystem();
   const { get, set } = useStorage();
     const { getPhoto } = useCamera();
+    const dispatch = useDispatch()
+    const memoryFull = useSelector((state:RootState) => state.memoryFull)
+    
     const [photos, setPhotos] = useState<Photo[]>([]);
     const [newPhotosTaken, setNewPhotos] = useState<Photo[]>([])
     const savePicture = async (photo: CameraPhoto, fileName: string): Promise<Photo> => {
@@ -75,13 +101,47 @@ export function usePhotoGallery() {
       loadSaved();
     }, [get, readFile]);
     const takePhoto = async () => {
+      console.log(Object.values(memoryFull)[0].memoryFull)
+      if(Object.values(memoryFull)[0].memoryFull==false){
+        console.log("here")
       const cameraPhoto = await getPhoto({
         resultType: CameraResultType.Uri,
         source: CameraSource.Camera,
         quality: 100
       });
-    
       const fileName = new Date().getTime() + '.jpeg';
+      const savedFileImage = await savePicture(cameraPhoto, fileName);
+      setNewPhotos([savedFileImage, ...newPhotosTaken])
+      const newPhotos = [...photos, savedFileImage, ];
+      setPhotos(newPhotos);
+      
+        console.log("trying")
+        let x:Promise<void>
+      x = set(PHOTO_STORAGE, JSON.stringify(newPhotos));
+
+      if(!x){
+        console.log("hello")
+        throw("Exception")
+      }else{
+        promiseState(x, function(state) {
+          
+          if(state=='rejected'){
+            dispatch(setTrue(true))
+            //console.log("reject")
+            //throw("Exception: Memory")
+          }
+          })
+          
+      
+    
+        console.log( x)
+        console.log(Object.values(x))
+      }
+    }else{
+      const cameraPhoto = []
+    }
+    
+      
       /*const newPhotos = [{
         filepath: fileName,
         webviewPath: cameraPhoto.webPath
@@ -92,11 +152,8 @@ export function usePhotoGallery() {
       }, ...photos];
       setPhotos(newPhotos)*/
       
-      const savedFileImage = await savePicture(cameraPhoto, fileName);
-      setNewPhotos([savedFileImage, ...newPhotosTaken])
-      const newPhotos = [...photos, savedFileImage, ];
-      setPhotos(newPhotos);
-      set(PHOTO_STORAGE, JSON.stringify(newPhotos));
+      
+      
     };
     const clearPhotos=()=>{
       setPhotos([])
