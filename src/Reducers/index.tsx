f5bbf8efe1441483a59e7ff4652dc539d10d6bda
Victoria ❤ <vickyakeke@gmail.com>
2021-02-09 22:14:07 +0300

    Adding in the memory full check as a state

--- src/Reducers/index.tsx
+++ src/Reducers/index.tsx
@@ -6,12 +6,15 @@ import recordsReducer from './RecordsReducer'
 import photosReducer from './PhotosReducer'
 import walksBeforeReducer from './WalksBeforeReducer'
 import walksReducer from './WalksReducer'
-const rootReducer = combineReducers({
+import memoryFull from './MemoryReducer'
+export const rootReducer = combineReducers({
   species: speciesReducer,
   records: recordsReducer,
   photos: photosReducer, 
   walk: walksBeforeReducer,
   walks: walksReducer,
+  memoryFull: memoryFull,
 })
 
 export default rootReducer
+export type RootState = ReturnType<typeof rootReducer>
