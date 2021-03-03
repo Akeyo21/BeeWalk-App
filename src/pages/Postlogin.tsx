60e2da547d81edb1d4a6f61e4bfb459d7681827a
Victoria ❤ <vickyakeke@gmail.com>
2021-03-03 22:02:18 +0300

    Common bees visible in the area

--- src/pages/Postlogin.tsx
+++ src/pages/Postlogin.tsx
@@ -1,7 +1,7 @@
 import { IonContent, IonHeader, IonPage, IonButton, IonRouterOutlet , IonTabs, IonTabBar,IonTabButton,
 IonIcon, IonLabel, IonBadge, IonSlides, IonSlide, IonText} from '@ionic/react';
 import { calendar, personCircle, map, informationCircle, ellipsisHorizontal, home, leaf, navigate, walk } from 'ionicons/icons';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 /*import './Home.css';*/
 import '../components/ExploreContainer.css';
 import '../components/LoginPage.css';
@@ -16,13 +16,63 @@ const Postlogin: React.FC = () => {
     initialSlide: 1,
     speed: 400
   };
-  console.log(data[15])
-  console.log(bee_data[20][10][6])
-  for (var elem in bee_data[20][10][6]){
-    if (bee_data[20][10][6][elem]>0){
-      console.log(elem)
+const [currentLong, setLong] = useState(0)
+const [ currrentLat, setLat] = useState(0)
+  //check if current location is in the UK
+  const inUK = (lat:number, long:number)=>{
+    if(lat<40 || lat>60){
+        throw new Error("Not in the UK-lat")
+    }else if (long<-10|| long>20){
+        throw new Error("Not in the UK- long")
+    }else{
+        return "In the UK"
     }
   }
+
+
+//change index
+const latLongIndex = (pos:any, start:number, intervals:number)=>{
+    let difference = pos - start;
+    let division = difference/0.5;
+    return division-1;
+}
+const latStart = 40
+const longStart = -10
+const interval = 0.5
+const [commonBees, setbees] = useState([])
+const [notInUK, setnotInUK] = useState(false)
+//check if current location is in the UK or not
+const giveIndex=(lat:number, long:number)=>{
+  try{
+    inUK(lat, long)
+    //setbees(bee_data[20][10][6].filter((species: number)=>species>0&&species!=26))
+  }
+  catch(error){
+    setnotInUK(true)
+  }
+}
+var today = new Date();
+navigator.geolocation.getCurrentPosition(function(position) {
+  setLat(position.coords.latitude)
+  setLong(position.coords.longitude)
+  let long = position.coords.longitude
+  let lat = position.coords.latitude
+  /*console.log(typeof long)
+  console.log("Latitude is :", typeof position.coords.latitude);
+  console.log("Longitude is :", position.coords.longitude);*/
+  try{
+    inUK(lat, long)
+    setbees(bee_data[latLongIndex(lat, latStart, interval)][latLongIndex(long, longStart, interval)][today.getMonth()].filter((species: number)=>species>0&&species!=26))
+  }
+  catch(error){
+    setnotInUK(true)
+    setbees([])
+  }
+});
+
+  //giveIndex(currrentLat, currentLong)
+  
+
  
   const dispatch = useDispatch()
  const reset=()=>{
@@ -69,14 +119,18 @@ const Postlogin: React.FC = () => {
                 <IonLabel>More</IonLabel>
               </IonTabButton>
             </IonTabBar>
-            <IonSlides pager={true} options={slideOpts} className="black">
-              {bee_data[20][10][6].map((species:any)=>(
-                (species>0 && species!=26)?
+            {notInUK? <h4 className="black">Common bees not featured in your country</h4> 
+              : commonBees.length==0? <h4 className="black">Not as many bees at this time of year</h4> :
+                <IonSlides pager={true} options={slideOpts} className="black">
+                  {commonBees.map((species:any)=>(
                   <IonSlide>
                   <h4>{data[species]}</h4>
-                  </IonSlide>:<IonText></IonText>
+                  </IonSlide>
+                
+                  
               ))}
-                </IonSlides>
+              </IonSlides>}
+            
                 <div id="move">
                 
                     <IonButton routerLink="/start/prewalk"
