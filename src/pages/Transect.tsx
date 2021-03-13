b879cc5d86a360ee5242eb2013941f7d3108e3ba
Victoria ❤ <vickyakeke@gmail.com>
2021-03-13 03:00:15 +0300

    Finish up on setting the transects up on the map manually

--- src/pages/Transect.tsx
+++ src/pages/Transect.tsx
@@ -3,8 +3,10 @@ import '../components/ExploreContainer.css';
 import {IonAlert, IonButton, IonContent, IonHeader, IonIcon, IonLabel, IonPage, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs} from '@ionic/react';
 import { useDispatch } from 'react-redux';
 import { Loader } from '@googlemaps/js-api-loader';
-import { home, walk, leaf, navigate, ellipsisHorizontal } from 'ionicons/icons';
-import { Section } from '../Reducers/RouteStartReducer';
+import { Section } from '../Reducers/SectionsReducer';
+import { Redirect, Route } from 'react-router';
+import SectionDetails from './SectionDetails';
+import { setSections } from '../Actions/Transect';
 
 interface ContainerProps { 
   
@@ -16,8 +18,9 @@ const Transect: React.FC<ContainerProps> = () => {
     const [long,  setlong] = useState(0)  
     const[filled, setfilled] = useState(false)
 
-    const [sections, setSections] = useState([]);
-    const [currentState, setState]= useState({currentDistance:0, startSection:0});
+    const [sections, changeSections] = useState([]);
+    const [emptySection, setEmptySections]= useState(false);
+    const [redirectToSectionDetails, setRedirectToDetails] = useState(false);
     //prompts user to scroll to the position if navigation
     //fails
     const [showScrollToPos, setScrollToPos] = useState(true)
@@ -35,17 +38,21 @@ const Transect: React.FC<ContainerProps> = () => {
         //the buttons to be included on the map
         const addSectionButton = document.createElement("button");
         addSectionButton.innerHTML = "Add Section";
-        addSectionButton.style.position = "relative";
-        addSectionButton.style.top = "10%";
-        addSectionButton.style.padding="20% 15%";
+        addSectionButton.style.padding="8% 10%";
         addSectionButton.style.backgroundColor = "white";
         addSectionButton.style.color = "black";
-        const buttonDiv = document.createElement("div")
-        buttonDiv.appendChild(addSectionButton)
+        
+        const fillSectionButton = document.createElement("button");
+        fillSectionButton.innerHTML = "Fill Section details";
+        fillSectionButton.style.padding="10% 3%";
+        fillSectionButton.style.margin="10% 0";
+        fillSectionButton.style.backgroundColor = "white";
+        fillSectionButton.style.color = "black";
         
         div.appendChild(addSectionButton);
-
+        div.appendChild(fillSectionButton);
         addSectionButton.addEventListener("click",saveSection)
+        fillSectionButton.addEventListener("click", addSections)
         
     }
     const loader = new Loader({
@@ -54,7 +61,8 @@ const Transect: React.FC<ContainerProps> = () => {
       console.log("here")
       let map: google.maps.Map;
       let poly: google.maps.Polyline;
-      loader.load().then(() => {
+      loader.load()
+      .then(() => {
           console.log("map should be here")
         if (filled){
             
@@ -98,32 +106,55 @@ const Transect: React.FC<ContainerProps> = () => {
                 }
             }
         });
-      });
+    });
 
-      
-      function addLatLng(event: google.maps.MapMouseEvent) {
+    
+    function addLatLng(event: google.maps.MapMouseEvent) {
         const path = poly.getPath();
       
         // Because path is an MVCArray, we can simply append a new coordinate
         // and it will automatically appear.
         path.push(event.latLng);
       
+        let contentString = '<div id="dark-text"><p>Section '+(sections.length+1) +'</p></div>'
+    const infowindow = new google.maps.InfoWindow({
+      content: contentString,
+    });  
         // Add a new marker at the new plotted point on the polyline.
-        /*new google.maps.Marker({
-          position: event.latLng,
-          title: "#" + path.getLength(),
+        let marker = new google.maps.Marker({
+          position: path.getAt(0),
+          title: "#" + sections.length,
           map: map,
-        });*/
+        });
+        marker.addListener("click", () => {
+          infowindow.open(map, marker);
+        });
     }
 
     //add button to set up the sections
+    //ensure that a section has been entered
     //go to allow for details on habitat and land use 
     //to be updated
     //finish setting up transect
-    function saveSection(event: any){        
+    const dispatch = useDispatch()
+    const addSections =()=>{
+      saveSection()
+        //check there is a section added
+        if(sections.length==0){
+            setEmptySections(true);
+        }else{
+         
+            dispatch(setSections(sections));
+            setRedirectToDetails(true);
+        }
+    }
+    function saveSection(){   
+           
         const path = poly.getPath()
         const distance = google.maps.geometry.spherical.computeLength(path);
-
+        
+        //ensure that an actual section has bee entered
+        if(path.getArray().length>0){
         let section: Section;
         section=new Section(distance, "0", [],[],path.getArray())        
         console.log(path, ' ',google.maps.geometry.spherical.computeLength(path))
@@ -134,9 +165,9 @@ const Transect: React.FC<ContainerProps> = () => {
 
         //generate different colors for the different poylines
         let colors = ['red', 'maroon', 'blue','silver', 'yellow','olive', 'lime', 'purple', 'orange', 'green',
-    'pink', 'brown', 'coral', 'magenta', 'tan']
+        'pink', 'brown', 'coral', 'magenta', 'tan']
         let randomColor =Math.floor(Math.random()*colors.length-1)
-
+        
         //new polyline for different sections
         poly=new google.maps.Polyline({
             path:[last],
@@ -146,9 +177,29 @@ const Transect: React.FC<ContainerProps> = () => {
             editable:true
           });
         poly.setMap(map);
-            
+        //remove an edge of the transect when setting up
+        poly.addListener("dblclick", function giveOptions(event: google.maps.MapMouseEvent){
+            const path = poly.getPath();
+            console.log(path)
+            let pathlist = path.getArray();
+            let reduced = pathlist.filter(item=>item!==event.latLng)
+            for (let i=0; i<path.getLength();i++){
+                if( path.getAt(i) == event.latLng){
+                    path.removeAt(i)
+                }
+            }
+        });
+        console.log(sections) 
+        }    
+    }
+    if(redirectToSectionDetails ==true){
+        return <Redirect to="/sectiondetails"/>
     }
   return (
+    <><IonRouterOutlet>
+    <Route path="/sectiondetails" component={SectionDetails} />
+
+</IonRouterOutlet>
     <IonPage >
         <IonHeader>
         </IonHeader>
@@ -167,10 +218,23 @@ const Transect: React.FC<ContainerProps> = () => {
                    
                   }
                 ]} />
+        
+        <IonAlert
+                isOpen={emptySection}
+                onDidDismiss={() => setEmptySections(false)}
+                cssClass='submitalert'
+                header={'Empty sections'}
+                message={'Enter a section first'}
+                buttons={[
+                  {
+                    text: 'OK',
+                   
+                  }
+                ]} />
 
     </div>
     </IonContent>
-    </IonPage>
+    </IonPage></>
     
   );
 };
