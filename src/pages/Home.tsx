9cf7bf38bc432cd8b18a247b69b98ac2bab7e9a2
Victoria ❤ <vickyakeke@gmail.com>
2021-04-13 04:30:45 +0300

    Commenting out code to rmeove calling api out

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,19 +11,69 @@ import Tabs from '../components/Tabs';
 import data from '../beeInfo/species_set.json';
 import bee_data from  '../beeInfo/bee_data.json';
 import CommonBees from '../components/CommonBees';
+
 /**/
 const Postlogin: React.FC = () => {
   //const reader = new FileReader();
-  //let data ={'image1': ('bumblebee.jpg', reader.readAsDataURL('bumblebee.jpg'))}
+  let filepath:any = './bumblebee.jpg';
+  let file = 'bumblebee.jpg';
+  /*reader.addEventListener("load", function () {
+    // convert image file to base64 string
+    filepath= reader.result;
+  }, false);
+  //var reader = new FileReader();
+  reader.onloadend = function() {
+    filepath= reader.result;
+    console.log('RESULT', reader.result)
+  }
+  let blob;
+  if(file){
+    blob= reader.readAsDataURL(filepath);
+  }else{
+    console.log("error")
+  }*/
+  function toDataURL(url: string, callback: { (dataUrl: any): void; (arg0: string | ArrayBuffer | null): void; }) {
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+      var reader = new FileReader();
+      reader.onloadend = function() {
+        callback(reader.result);
+      }
+      reader.readAsDataURL(xhr.response);
+    };
+    xhr.open('GET', url);
+    xhr.responseType = 'blob';
+    xhr.send();
+  }
+  let blob;
+  /*toDataURL(filepath, function(dataUrl: any) {
+    
+  let data ={'image1': file}
+    fetch('http://3.249.81.168/api/image',{
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'no-cors',
+    body: JSON.stringify(data)
+  })   
+  .then(response => response.text())
+  .then(data => {
+    console.log('Success:', data);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+    console.log('RESULT:', dataUrl)
+  })
+  console.log(blob);*/
+  
+  
+  let data ={'image1': [file, filepath]}
   /*useEffect(()=>{
-    fetch('http://3.249.81.168/api/image', {
-    method: 'POST', // or 'PUT'
-    /*headers: {
-      'Content-Type': 'application/json',
-    },
-    //body: JSON.stringify(data)
-    })
-  .then(response => response.json())
+  fetch('http://3.249.81.168/api/image',{
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'no-cors',
+    body: JSON.stringify(data)
+  })   
+  .then(response => console.log(response.text()))
   .then(data => {
     console.log('Success:', data);
   })
@@ -44,7 +94,6 @@ const Postlogin: React.FC = () => {
           <div className="container">
             
               <div className="wholepage" >      
-            
                 <CommonBees/>
                 <div id="move">
                 
