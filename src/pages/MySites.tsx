1b4d4c00908942845b6edf318d56a8997afaeeef
Victoria ❤ <vickyakeke@gmail.com>
2021-04-19 03:21:00 +0300

    Ensure variables are not null on load

--- src/pages/MySites.tsx
+++ src/pages/MySites.tsx
@@ -1,31 +1,68 @@
-import { IonContent, IonHeader, IonPage, IonRouterOutlet, IonList, IonItem, IonIcon, IonLabel, IonNote, IonListHeader, IonButton, IonCol, IonGrid, IonRow, IonTabBar, IonTabButton, IonItemSliding, IonItemOption, IonItemOptions, IonAlert } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonRouterOutlet, IonList, IonItem, IonIcon, IonLabel, IonNote, IonListHeader, IonButton, IonCol, IonGrid, IonRow, IonTabBar, IonTabButton, IonItemSliding, IonItemOption, IonItemOptions, IonAlert, useIonAlert } from '@ionic/react';
 import React, { useState } from 'react';
 
 import './Default.css';
 import Login from "../pages/Login";
 import AddSite from "../pages/AddSites";
-import {Route} from 'react-router-dom';
+import {Redirect, Route} from 'react-router-dom';
 import { home, walk, leaf, navigate, ellipsisHorizontal } from 'ionicons/icons';
 import { connect, useStore } from 'react-redux';
 import Tabs from '../components/Tabs';
 /*MySites - gives a page that shows a list of sites the user has
 entered*/
 interface ContainerProps { 
-  transects:[]
+  transects:[],
+  sections:[]
 }
 
 /*Form that is rendered when the manual adding of bee data
 is selected*/
 const MySites: React.FC<ContainerProps> = (props) => {
+  const[addSite, setAddSite] = useState(false)
+  const[goToTransect, setGoToTransect] = useState(false)
   let transectslist: any[] =[]
   for(const property in props.transects){
    transectslist = props.transects[property]
    
  }
+ 
+ let sectionslist: any[] =[]
+  for(const property in props.sections){
+   sectionslist = props.sections[property]   
+ }
+ console.log(sectionslist)
  const [showWarning, setShowWarning] = useState(false);
   //const [proceedDelete, setProceedDelete] = useState(false);
  const deleteTransect=(key:any)=>{
    
+ }
+
+ const [present] = useIonAlert(); 
+ const checkTransects=()=>{
+   if(sectionslist.length>0){
+    present({
+      cssClass: 'my-css',
+      header: 'Incomplete Transect',
+      message: 'Save sections in the new transect entered',
+      buttons:[ {
+       text: 'OK',
+      handler(){
+        setGoToTransect(true)
+        
+        //deleteTransect(showWarning[1]);
+      }
+     }, 'Cancel'],
+      onDidDismiss: (e) => console.log(e) ,
+    })
+   }else{
+     setAddSite(true)
+   }
+ }
+ if(addSite){
+   return <Redirect to="/mysites/add"/>
+ }
+ if(goToTransect){
+  return <Redirect to="/transect"/>
  }
   return (
     <><><IonRouterOutlet>
@@ -55,9 +92,14 @@ const MySites: React.FC<ContainerProps> = (props) => {
             <div className="sites">
            
                 <IonList lines="full" className="list">
-                    <IonListHeader lines="full" className="whitebackground" id="header" >My Sites
-                    
-                <IonButton className="site-button" href="/mysites/add">Add Site</IonButton>
+                    <IonListHeader lines="full" className="whitebackground" id="header" >
+                      <h1 className="bold">My Sites</h1>
+                    {sectionslist? 
+                sectionslist.length>0?
+                 <IonButton className="site-button" href="/transect">Finish Adding Site</IonButton>:
+                 <IonButton className="site-button" href="/mysites/add">Add Site</IonButton>: 
+                 <IonButton className="site-button" href="/mysites/add">Add Site</IonButton>}    
+                
                     </IonListHeader>
                     {transectslist.length==0?<h1 className="dark">No Transects have been set up</h1>:
                     transectslist.map((transect, index)=>(
@@ -91,7 +133,8 @@ const MySites: React.FC<ContainerProps> = (props) => {
 };
 const mapStateToProps = function(state: any) {
   return {
-    transects:state.transects
+    transects:state.transects,
+    sections:state.sections
   }
 }
 export default connect(mapStateToProps)(MySites);/*
