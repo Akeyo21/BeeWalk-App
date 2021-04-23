7221eacf14e78e1e53a4b45e3deab2037f93bc55
Victoria ❤ <vickyakeke@gmail.com>
2021-04-24 01:21:59 +0300

    Each card is a common bee in the area

--- src/components/BeesInArea.tsx
+++ src/components/BeesInArea.tsx
@@ -3,14 +3,20 @@ import './ExploreContainer.css';
 import {IonInput,IonButton, IonCard, IonCardContent, IonCol, IonGrid, IonRow} from '@ionic/react';
 import './LoginPage.css';
 import "../pages/Default.css";
-
-
+import {allBees, Bee} from '../beeInfo/DifferentBeeSpecies';
 interface ContainerProps { 
-  
+  name:String
 }
 
-const BeesInArea: React.FC<ContainerProps> = () => {
+const BeesInArea: React.FC<ContainerProps> = (props) => {
   /*<input type="submit" value="Log In" id="submit"></input>*/
+  let currentBee:Bee|null= null
+  for(let i=0;i<allBees.length;i++){
+    if (props.name.toLowerCase()==allBees[i].common.toLowerCase()){
+        currentBee = allBees[i]
+    }
+  }
+  console.log(currentBee)
   return (   
 
        
@@ -19,13 +25,19 @@ const BeesInArea: React.FC<ContainerProps> = () => {
         <IonCardContent className="nopadding">
         <IonGrid className="nopadding">
             <IonRow>
+                {currentBee? 
+                <>
                 <IonCol size="8" className="nopadding">
-                    <img src="assets/images/bee.jpg"></img>
+                    <img src={currentBee.getImage()}></img>
                 </IonCol>
 
-                <IonCol size="4">
-                    <h3>Bee Species name</h3>
-                </IonCol>
+                              <IonCol size="4">
+                                  <h3>{currentBee.getCommon()}</h3>
+                                  <h5 className="italics">{currentBee.getScientific()}</h5>
+                              </IonCol></>: 
+                              
+                              <IonCol size="12">
+                                  <h2>{props.name}</h2></IonCol>}
             </IonRow>
         </IonGrid>
         </IonCardContent>
