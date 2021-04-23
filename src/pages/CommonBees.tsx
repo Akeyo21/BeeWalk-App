a4fba1ae6a85f2ca2a1c1cb291f002061a954f7f
Victoria ❤ <vickyakeke@gmail.com>
2021-04-24 01:24:04 +0300

    Filling page with common bees in the area

--- src/pages/CommonBees.tsx
+++ src/pages/CommonBees.tsx
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../components/ExploreContainer.css';
-import { IonBackButton, IonButtons, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCol, IonContent, IonGrid, IonHeader, IonPage, IonRouterOutlet, IonRow, IonToolbar } from '@ionic/react';
+import { IonBackButton, IonButton, IonButtons, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCol, IonContent, IonGrid, IonHeader, IonPage, IonRouterOutlet, IonRow, IonToolbar } from '@ionic/react';
 
 import './Default.css';
 import BeesInArea from '../components/BeesInArea';
 import PreWalk from "../pages/PreWalk";
 import {Route} from 'react-router-dom';
+import { getBees } from '../beeInfo/Bees';
+import data from '../beeInfo/species_set.json';
 
 interface ContainerProps { 
 }
 /* CommonBees - page that shows the common bees in the are
 */
 const CommonBees: React.FC<ContainerProps> = (props) => {
+  const [commonBees, setbees] = useState([])
+  const [notInUK, setnotInUK] = useState(false) 
+  useEffect(()=>{
+    navigator.geolocation.getCurrentPosition(function(position) {
+        
+      let long = position.coords.longitude
+      let lat = position.coords.latitude
+      try{
+      
+      setbees(getBees(lat, long))
+      }
+      catch(error){   
+        console.log(error) 
+        setnotInUK(true)
+      }
+    })
+  },[])
+  console.log(commonBees)
   return (
+    
     <><><IonRouterOutlet>
       <Route path="/start/prewalk" component={PreWalk} />
     </IonRouterOutlet></>
@@ -23,15 +44,18 @@ const CommonBees: React.FC<ContainerProps> = (props) => {
           <IonHeader >
           </IonHeader>
           <div className="container">
-            
+          <IonButton href="/"className="back whitebackground" >Back</IonButton>
+         
         <div className="wholepage "> 
-        <IonBackButton defaultHref="/frontpage" icon="buttonIcon" text="BACK" className="ion-float-left" color="dark"/><br/>
-          <BeesInArea />
-          <BeesInArea />
-          <BeesInArea />
-          <BeesInArea />
-          <BeesInArea />
-            <BeesInArea />
+        <div className="beelist">
+        {notInUK? <h4 className="black">Common bees not featured in your country</h4> 
+              : commonBees.length==0? <h4 className="black">Not as many bees at this time of year</h4> :
+                  commonBees.map((species:any, index:number)=>(
+                  <BeesInArea key={index} name={data[species]}/>               
+                  
+              ))}
+        </div>
+           
         </div>
         </div>
           
