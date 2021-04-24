795c967a36a146519d433d59eb8f8d9799565631
Victoria ❤ <vickyakeke@gmail.com>
2021-04-24 03:06:00 +0300

    Allow null temporary state

--- src/Actions/temps.ts
+++ src/Actions/temps.ts
@@ -5,7 +5,7 @@ import { Temps } from "../Reducers/temps"
 // Create Redux action types
 export const CHANGE_TEMP = 'CHANGE_TEMP'
 // Create Redux action creators that return an action
-export const changeTemp = (sectionNumber:Temps) => ({
+export const changeTemp = (sectionNumber:Temps|null) => ({
     type: CHANGE_TEMP,
     payload: sectionNumber,
   })
